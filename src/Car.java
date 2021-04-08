d61e060b4f54e6db03760bf05de5c2f034a26e63
trisha ann balderas <centrii52@gmail.com>
2021-04-08 19:23:07 +0800

    added feature car

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,10 @@
 public class Car {
+    int speed;
+    double regularPrice;
+    String color;
+    double getSalePrice;
+
+    public double getGetSalePrice() {
+        return getSalePrice;
+    }
 }
