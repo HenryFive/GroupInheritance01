21d47a08ac573d8659d7dacb29d4f96ada21e815
james-99k <jamesgasty@gmail.com>
2021-04-08 19:38:04 +0800

    jeepney class(updated)

--- src/Jeepney.java
+++ src/Jeepney.java
@@ -1,6 +1,11 @@
 public class Jeepney extends Car{
     int weight;
 
+    public Jeepney(int speed, double regularPrice, String color, double getSalePrice, int weight) {
+        super(speed, regularPrice, color, getSalePrice);
+        this.weight = weight;
+    }
+
     double getSalePrice(){
         if (weight > 2000){
             return regularPrice * 0.10;
