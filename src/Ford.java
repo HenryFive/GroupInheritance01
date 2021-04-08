4e3dbe8c7b28a470fd1d431f3e6437611e3fb958
BaleteJ <balete.jason27@gmail.com>
2021-04-08 19:16:28 +0800

    Added skeleton code for Ford class

--- src/Ford.java
+++ src/Ford.java
@@ -1,2 +1,11 @@
-public class Ford {
+public class Ford{
+    protected int year;
+    protected int manufacturerDiscount;
+
+    double getSalePrice()
+    {
+        //From sale price computed from car class, subtract the manufacturerDiscount
+        return this.manufacturerDiscount -= 0;
+    }
+
 }
